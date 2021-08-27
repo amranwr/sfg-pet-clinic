c705230099d49ffa8d4e53bd9dbaff3b57198504
amranwr <amrtop2000@gmail.com>
2021-08-27 20:09:26 +0200

    adding pet clinic index and controller. closes #7.

---
+++ pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package amr.springproject.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index.html"})
+    public String getIdex(){
+        return "index";
+    }
+}
