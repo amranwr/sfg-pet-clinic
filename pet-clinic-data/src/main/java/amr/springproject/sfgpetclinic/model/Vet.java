5cb9accee4810d0b04ebb4852703798352a8f29b
amranwr <amrtop2000@gmail.com>
2021-10-19 17:26:55 +0200

    create visit and specialty entitiy . closes #22

--- pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Vet.java
@@ -1,3 +1,15 @@
 package amr.springproject.sfgpetclinic.model;
+
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
