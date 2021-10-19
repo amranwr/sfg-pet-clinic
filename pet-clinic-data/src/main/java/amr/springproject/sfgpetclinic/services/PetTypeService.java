a39195055dd8c3316d53f0347e181fb5daadcde2
amranwr <amrtop2000@gmail.com>
2021-10-19 17:34:12 +0200

    create pet type map service . closes #23

---
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package amr.springproject.sfgpetclinic.services;
+
+import amr.springproject.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType,Long>{
+}
---
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package amr.springproject.sfgpetclinic.services.map;
+
+import amr.springproject.sfgpetclinic.model.PetType;
+import amr.springproject.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends MapService<PetType,Long> implements PetTypeService {
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
