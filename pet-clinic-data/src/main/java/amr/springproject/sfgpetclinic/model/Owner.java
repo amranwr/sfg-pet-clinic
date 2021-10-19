0a62a29b7bbbc384a8587a739e87c066405a468a
amranwr <amrtop2000@gmail.com>
2021-10-19 17:19:23 +0200

    update owner entity and creating visite entity . closes #21

--- pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Owner.java
@@ -1,3 +1,15 @@
 package amr.springproject.sfgpetclinic.model;
+
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
