be832f929f78e0aaaab9641bb956d51554fc566d
amranwr <amrtop2000@gmail.com>
2021-10-20 08:42:33 +0200

    add pet type to bootstrap . closes #24

--- pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/bootStrap/DataLoader.java
+++ pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/bootStrap/DataLoader.java
@@ -1,26 +1,40 @@
 package amr.springproject.sfgpetclinic.bootStrap;
 
 import amr.springproject.sfgpetclinic.model.Owner;
+import amr.springproject.sfgpetclinic.model.Pet;
+import amr.springproject.sfgpetclinic.model.PetType;
 import amr.springproject.sfgpetclinic.model.Vet;
 import amr.springproject.sfgpetclinic.services.OwnerService;
+import amr.springproject.sfgpetclinic.services.PetTypeService;
 import amr.springproject.sfgpetclinic.services.VetService;
 import amr.springproject.sfgpetclinic.services.map.OwnerMapService;
 import amr.springproject.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerMapService ownerService, VetServiceMap vetService) {
+
+    public DataLoader(OwnerMapService ownerService, VetServiceMap vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog=  new PetType();
+        dog.setName("kong");
+        this.petTypeService.save(dog);
+
+
         System.out.println("ana fen");
         Owner owner = new Owner();
         owner.setFirstName("amr");
