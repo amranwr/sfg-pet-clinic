cb7321fbe565c71568b44f3f8849807e86e0f01e
amranwr <amrtop2000@gmail.com>
2021-10-21 10:39:55 +0200

    converting visit to jpa entity. closes #31

--- pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Pet.java
@@ -1,12 +1,17 @@
 package amr.springproject.sfgpetclinic.model;
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name = "owner_id")
@@ -15,6 +20,14 @@ public class Pet extends BaseEntity{
     private LocalDate birthDate;
     @Column(name = "name")
     private String name;
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
--- pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/amr/springproject/sfgpetclinic/model/Visit.java
@@ -1,10 +1,16 @@
 package amr.springproject.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "visits")
 public class Visit extends  BaseEntity{
+    @Column(name = "localdate")
     private LocalDate localDate;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getLocalDate() {
