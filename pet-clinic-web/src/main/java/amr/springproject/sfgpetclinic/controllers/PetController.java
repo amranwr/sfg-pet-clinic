a2dc1427a729b6f4bc4d4b2fe2c1fea180ee470c
amranwr <amrtop2000@gmail.com>
2021-11-05 11:13:25 +0200

    adding and updating owner and its tests #18

---
+++ pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,43 @@
+package amr.springproject.sfgpetclinic.controllers;
+
+import amr.springproject.sfgpetclinic.model.Owner;
+import amr.springproject.sfgpetclinic.model.PetType;
+import amr.springproject.sfgpetclinic.services.OwnerService;
+import amr.springproject.sfgpetclinic.services.PetService;
+import amr.springproject.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+@RequestMapping("/owners/{ownerId}")
+@Controller
+public class PetController {
+    private OwnerService ownerService;
+    private PetService petService;
+    private PetTypeService petTypeService;
+
+    public PetController(OwnerService ownerService, PetService petService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
