397f536917bc95419f3c79b6c8dc1d51c51e575b
amranwr <amrtop2000@gmail.com>
2021-11-10 13:57:40 +0200

    making json response of vets close #57

--- pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/amr/springproject/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package amr.springproject.sfgpetclinic.controllers;
 
+import amr.springproject.sfgpetclinic.model.Vet;
 import amr.springproject.sfgpetclinic.services.VetService;
 import amr.springproject.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -16,5 +21,10 @@ public class VetController {
     public String getVetIndex(Model model){
         model.addAttribute("vets",this.vet.findAll());
         return "vet/index";
+    }
+
+    @GetMapping("api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON(){
+        return vet.findAll();
     }
 }
